185013026fee5c3802684373d84da7de1c314418
saf <safwaanshaikh21@gmail.com>
2023-05-04 01:10:03 -0400

    added some basic files. login + registration.

---
+++ src/components/InitializeFirebase.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDEqTKxK5oyE-imci4Q5o1nR03w2r0Mk4A",
+    authDomain: "group-networking-a1b6f.firebaseapp.com",
+    projectId: "group-networking-a1b6f",
+    storageBucket: "group-networking-a1b6f.appspot.com",
+    messagingSenderId: "887810659767",
+    appId: "1:887810659767:web:ffb808fde06f06bad28432",
+    measurementId: "G-D9PDCCBHV5"
+  };
+  
+const app = initializeApp(firebaseConfig);
+
+export default app;
