0837667216e27e427167b7dea9e76e6b996efc9c
Zi Kai Xu <kai@kais-mbp.home>
2023-05-04 17:45:01 -0400

    landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,28 @@ import localfont from '@next/font/local';
 const poppins = localfont({
   src: [
     {
-      path: '../../public/fonts/AirbnbCereal_W_Bd.otf',
+      path: '../../public/fonts/AirbnbCereal_W_Lt.otf',
+      weight: '300'
+    },
+    {
+      path: '../../public/fonts/AirbnbCereal_W_Bk.otf',
       weight: '400'
+    },
+    {
+      path: '../../public/fonts/AirbnbCereal_W_Md.otf',
+      weight: '500'
     },
     {
       path: '../../public/fonts/AirbnbCereal_W_Bd.otf',
       weight: '700'
+    },
+    {
+      path: '../../public/fonts/AirbnbCereal_W_XBd.otf',
+      weight: '800'
+    },
+    {
+      path: '../../public/fonts/AirbnbCereal_W_Blk.otf',
+      weight: '900'
     }
   ],
   variable: '--font-poppins'
